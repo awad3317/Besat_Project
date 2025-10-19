1c048188096e038474d8b4d43aacb2b460877341
awad3317 <127610663+awad3317@users.noreply.github.com>
2025-10-19 12:46:55 +0300

    Update 2025_10_14_110105_create_requests_table.php

--- database/migrations/2025_10_14_110105_create_requests_table.php
+++ database/migrations/2025_10_14_110105_create_requests_table.php
@@ -17,7 +17,6 @@ return new class extends Migration
             $table->foreignId('driver_id')->nullable()->constrained()->onDelete('set null');
             $table->foreignId('discount_code_id')->references('id')->on('discount_codes')->onDelete('set null');
             $table->foreignId('service_id')->constrained()->onDelete('cascade');
-            $table->string('discount_code_id', 50)->nullable();
             $table->decimal('start_latitude', 10, 8);
             $table->decimal('start_longitude', 10, 8);
             $table->string('start_address');
