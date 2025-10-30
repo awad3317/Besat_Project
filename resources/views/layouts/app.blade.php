94a7f6f5e32231f495ef4106e315323dfde29116
awad3317 <127610663+awad3317@users.noreply.github.com>
2025-10-30 16:48:29 +0300

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1215,7 +1215,7 @@
             <div class="grid grid-cols-12 gap-4 md:gap-6">
               <div class="col-span-12 space-y-6 xl:col-span-7">
                 <!-- Metric Group One -->
-                <div class="flex flex-row gap-4 md:gap-6">
+                <div class="flex flex-row gap-4 md:gap-2 ">
   <!-- Metric Item 1 - المستخدمين -->
   <div
     class="flex-1 rounded-2xl border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03] md:p-6"
