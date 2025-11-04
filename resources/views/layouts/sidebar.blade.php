392ac3981b0d969d2cf4dacf16b0c4ddb34e8edd
awad3317 <127610663+awad3317@users.noreply.github.com>
2025-11-04 16:54:21 +0300

    Update sidebar.blade.php

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -174,33 +174,13 @@
               class="menu-item group"
               :class="window.location.href.includes('{{ route('request.index') }}') ? 'menu-item-active' : 'menu-item-inactive'"
             >
-              {{-- <svg :class="window.location.href.includes('{{ route('request.index') }}') ? 'menu-item-icon-active' : 'menu-item-icon-inactive'" fill="#dc6803" width="30" height="30" viewBox="0 0 32 32" id="icon"
-                        xmlns="http://www.w3.org/2000/svg">
-                        <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-                        <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
-                        <g id="SVGRepo_iconCarrier">
-                            <defs>
-                                <style>
-                                    .cls-1 {
-                                        fill: none;
-                                    }
-                                </style>
-                            </defs>
-                            <title>bus</title>
-                            <rect x="27" y="11" width="2" height="4"></rect>
-                            <rect x="3" y="11" width="2" height="4"></rect>
-                            <rect x="20" y="20" width="2" height="2"></rect>
-                            <rect x="10" y="20" width="2" height="2"></rect>
-                            <path
-                                d="M21,4H11A5.0059,5.0059,0,0,0,6,9V23a2.0023,2.0023,0,0,0,2,2v3h2V25H22v3h2V25a2.0027,2.0027,0,0,0,2-2V9A5.0059,5.0059,0,0,0,21,4Zm3,6,.0009,6H8V10ZM11,6H21a2.995,2.995,0,0,1,2.8157,2H8.1843A2.995,2.995,0,0,1,11,6ZM8,23V18H24.0012l.0008,5Z"
-                                transform="translate(0 0)"></path>
-                            <rect id="_Transparent_Rectangle_" data-name="&lt;Transparent Rectangle&gt;" class="cls-1"
-                                width="32" height="32">
-                            </rect>
-                        </g>
-                    </svg> --}}
 
-                    <svg  :class="window.location.href.includes('{{ route('request.index') }}') ? 'menu-item-icon-active' : 'menu-item-icon-inactive'" fill="#dc6803" width="30" height="30" viewBox="0 0 32 32" id="icon" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><defs><style>.cls-1{fill:none;}</style></defs><title>request-quote</title><path d="M22,22v6H6V4H16V2H6A2,2,0,0,0,4,4V28a2,2,0,0,0,2,2H22a2,2,0,0,0,2-2V22Z" transform="translate(0)"></path><path d="M29.54,5.76l-3.3-3.3a1.6,1.6,0,0,0-2.24,0l-14,14V22h5.53l14-14a1.6,1.6,0,0,0,0-2.24ZM14.7,20H12V17.3l9.44-9.45,2.71,2.71ZM25.56,9.15,22.85,6.44l2.27-2.27,2.71,2.71Z" transform="translate(0)"></path><rect id="_Transparent_Rectangle_" data-name="&lt;Transparent Rectangle&gt;" class="cls-1" width="32" height="32"></rect></g></svg>
+              <svg  :class="window.location.href.includes('{{ route('request.index') }}') ? 'menu-item-icon-active' : 'menu-item-icon-inactive'" fill="#dc6803" width="30" height="30" viewBox="0 0 32 32" id="icon" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><defs><style>.cls-1{fill:none;}</style></defs>
+                <title>request</title>
+                <path d="M22,22v6H6V4H16V2H6A2,2,0,0,0,4,4V28a2,2,0,0,0,2,2H22a2,2,0,0,0,2-2V22Z" transform="translate(0)"></path><path d="M29.54,5.76l-3.3-3.3a1.6,1.6,0,0,0-2.24,0l-14,14V22h5.53l14-14a1.6,1.6,0,0,0,0-2.24ZM14.7,20H12V17.3l9.44-9.45,2.71,2.71ZM25.56,9.15,22.85,6.44l2.27-2.27,2.71,2.71Z" transform="translate(0)"></path><rect id="_Transparent_Rectangle_" data-name="&lt;Transparent Rectangle&gt;" class="cls-1" width="32" height="32">
+                  </rect>
+                </g>
+              </svg>
                     
 
               <span
