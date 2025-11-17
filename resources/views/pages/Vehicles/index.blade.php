355afe7332f8b0287ce8ca99791dfe21a62cadc6
awad3317 <127610663+awad3317@users.noreply.github.com>
2025-11-17 16:49:10 +0300

    update

---
+++ resources/views/pages/Vehicles/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+@section('title', 'لوحة التحكم')
+@section('Breadcrumb', 'إادارة المركبات')
+@section('style')
+
+@endsection
+@section('content')
+
+@endsection
+
+@section('script')
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\drivercontroller;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\requestcontroller;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\VehicleController;
 use App\Http\Controllers\SystemSettingsController;
 
 // Route::get('/', function () {
@@ -27,6 +28,7 @@ Route::middleware('auth')->group(function () {
     Route::resource('drivers', drivercontroller::class);
     Route::resource('request',requestcontroller::class);
     Route::resource('Coupon',couponcontroller::class);
+    Route::resource('Vehicle',VehicleController::class);
        Route::post('/system-settings/auto-assign', [SystemSettingsController::class, 'updateAutoAssignSetting'])
         ->name('system-settings.auto-assign.update');
         
