dba66627553f742deaee23047a32c85fe86451a0
awad3317 <127610663+awad3317@users.noreply.github.com>
2025-12-10 19:31:12 +0300

    Update firebase-notification.blade.php

--- resources/views/components/notification/firebase-notification.blade.php
+++ resources/views/components/notification/firebase-notification.blade.php
@@ -36,10 +36,11 @@
         <div class="rounded-xl border border-gray-200 bg-white p-4 shadow-lg dark:border-gray-700 dark:bg-gray-800">
             <div class="flex items-start gap-3">
                 <!-- الأيقونة على اليسار -->
-                <div class="flex-shrink-0 text-orange-600">
+                <div class="flex-shrink-0 bg-orange-50 text-orange-500">
                     <svg width="24" height="26" viewBox="0 0 24 26" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path
                             d="M12.814 4.75L4.78516 16.0352H11.1859L11.1859 23.25L19.2148 11.9648L12.814 11.9648V4.75Z"
+                            stroke="#465FFF"
                             stroke-width="1.5"
                             stroke-linecap="round"
                             stroke-linejoin="round"
