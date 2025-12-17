903a5e5ef95fc7931536c4f4eb811a892c9f1f06
awad3317 <127610663+awad3317@users.noreply.github.com>
2025-12-17 20:59:52 +0300

    Update index.blade.php

--- resources/views/pages/drivers/index.blade.php
+++ resources/views/pages/drivers/index.blade.php
@@ -45,7 +45,7 @@
             </div>
             <div class="mt-3 w-full">
                 <span class="text-xs text-gray-500 dark:text-gray-400">المحظورين</span>
-                <h4 class="mt-1 text-lg font-bold text-gray-800 dark:text-white/90">{{$bannedDrivers}}</h4>
+                <h4 class="mt-1 text-lg font-bold text-gray-800 dark:text-white/90">2</h4>
             </div>
         </div>
 
