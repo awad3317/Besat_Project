df1d497ee45f11eaad9d884437ef9b911403e9b0
awad3317 <127610663+awad3317@users.noreply.github.com>
2025-12-27 17:51:38 +0300

    Update show.blade.php

--- resources/views/pages/Vehicles/show.blade.php
+++ resources/views/pages/Vehicles/show.blade.php
@@ -14,7 +14,7 @@
   <div class=" mx-auto max-w-(--breakpoint-2xl)">
     <div class="flex flex-col sm:flex-row gap-4 md:gap-6 flex-wrap mb-4">
       <div
-        class="flex flex-col items-start justify-between rounded-xl border border-gray-200 bg-white p-4 dark:border-gray-800 dark:bg-white/[0.03] transition hover:shadow-md flex-1 min-w-[150px] sm:min-w-[180px] lg:min-w-[200px]">
+        class="flex flex-col items-start justify-between rounded-xl bg-white p-4 border border-brand-500 dark:border-brand-500 dark:bg-white/[0.03] transition hover:shadow-md flex-1 min-w-[150px] sm:min-w-[180px] lg:min-w-[200px]">
         <div class="flex h-10 w-10 items-center justify-center rounded-lg bg-gray-100 dark:bg-gray-800">
           <svg fill="#dc6803" width="24" height="24" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
             <rect x="27" y="11" width="2" height="4"></rect>
@@ -33,7 +33,7 @@
       </div>
 
       <div
-        class="flex flex-col items-start justify-between rounded-xl border border-gray-200 bg-white p-4 dark:border-gray-800 dark:bg-white/[0.03] transition hover:shadow-md flex-1 min-w-[150px] sm:min-w-[180px] lg:min-w-[200px]">
+        class="flex flex-col items-start justify-between rounded-xl bg-white p-4 border border-brand-500 dark:border-brand-500 dark:bg-white/[0.03] transition hover:shadow-md flex-1 min-w-[150px] sm:min-w-[180px] lg:min-w-[200px]">
         <div class="flex h-10 w-10 items-center justify-center rounded-lg bg-gray-100 dark:bg-gray-800">
           <svg fill="#dc6803" width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
             <path
