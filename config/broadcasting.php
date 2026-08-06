7c26954e14d041bc8039907faa1061f5ca8846d4
awad3317 <127610663+awad3317@users.noreply.github.com>
2026-08-06 14:56:10 +0300

    Update broadcasting.php

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -36,14 +36,13 @@ return [
             'secret' => env('REVERB_APP_SECRET'),
             'app_id' => env('REVERB_APP_ID'),
             'options' => [
-                'host' => env('REVERB_HOST'),
-                'port' => env('REVERB_PORT', 443),
-                'scheme' => env('REVERB_SCHEME', 'https'),
-                'useTLS' => env('REVERB_SCHEME', 'https') === 'https',
+                'host' => env('REVERB_SERVER_HOST', '127.0.0.1'),
+                'port' => env('REVERB_SERVER_PORT', 8085),
+                'scheme' => 'http',
+                'useTLS' => false,
             ],
             'client_options' => [
                 'verify' => false,
-                // Guzzle client options: https://docs.guzzlephp.org/en/stable/request-options.html
             ],
         ],
 
